538fd4f911bdfb144a858441ab851af6e861b3b9
Sandy <sandyforrest001@outlook.com>
2024-12-11 12:06:32 +0000

    Day 11 part 2: got the idea from hyper-neutrino but i might see if i can rewrite it another way

--- day11/solution2.py
+++ day11/solution2.py
@@ -1,63 +1,29 @@
 import os
 import time
-from multiprocessing import Pool
+from functools import cache
+
 
 currentPath = os.path.normpath(os.path.realpath(os.path.split(__file__)[0]))
 
-filename = os.path.join(currentPath, "testin.txt")
+filename = os.path.join(currentPath, "input.txt")
 
 with open(filename, "r") as f:
-    stones = [(s, False) for s in [line.strip() for line in f.readlines()][0].split(' ')]
+    stones = [int(s) for s in [line.strip() for line in f.readlines()][0].split(' ')]
 
-def process(stone: tuple[str, bool], index: int) -> tuple[int, str | tuple[str, str]]:
-    if stone[1]: return(index, stone[0])
+@cache
+def run(stone, steps):
+    if steps == 0:
+        return 1
 
-    if stone[0] == '0':
-        r = '1'
+    if stone == 0:
+        return run(1, steps - 1)
 
-    elif len(stone[0]) % 2 == 0:
-        sta = stone[0][:len(stone[0]) // 2]
-        end = stone[0][len(stone[0]) // 2:]
+    st = str(stone)
+    ln = len(st)
 
-        while end[0] == '0' and len(end) > 1:
-            end = end[1:]
+    if ln % 2 == 0:
+        return run(int(st[:ln // 2]), steps - 1) + run(int(st[ln // 2:]), steps  -1)
 
-        r = (sta, end)
+    return run(stone * 2024, steps - 1)
 
-    else:
-        r = str(int(stone[0]) * 2024)
-
-    return (index, r)
-
-for blink in range(25):
-    startTime = time.time()
-
-    stones = [(s[0], False) for s in stones]
-
-    pool = Pool()
-    args = []
-
-    returns = []
-
-    for ind, stone in enumerate(stones):
-        # returns.append(process(stone, ind))
-        args.append([stone, ind])
-
-    returns = pool.starmap(process, args)
-
-    for r in returns:
-        ind = r[0]
-        out = r[1]
-
-        if isinstance(out, str):
-            stones[ind] = (out, True)
-
-        else:
-            stones.insert(ind + 1, (out[1], True))
-            stones[ind] = (out[0], True)
-
-    print(f'Blink {blink + 1} of 75 complete. {len(stones)} stones in {round(time.time() - startTime, 3)} seconds.')
-
-print(len(stones))
-
-pass
+print(sum(run(stone, 75) for stone in stones))
